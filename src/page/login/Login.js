0b96d7a784e84bf96990426c20e8bb5c1939e266
Ricky Chettri <rickeykhd@gmail.com>
2021-04-06 16:13:52 +1000

    logoin form adde

--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import LoginForm from "../../components/layout/loginform/LoginForm";
+import "./login.style.css";
 const Login = () => {
-  return <div>hello login</div>;
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
