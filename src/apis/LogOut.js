ee25f1895fe6da4c0066ed88aecdc612f8e24e6e
skhadk10 <rickeykhd@gmail.com>
2021-06-12 14:00:40 +1000

    logout fixed

--- src/apis/LogOut.js
+++ src/apis/LogOut.js
@@ -4,6 +4,7 @@ const rootUrl = "http://localhost:8001/api/v1/";
 const catApi = rootUrl + "logOut";
 
 export const LogOutApi = (_id) => {
+  console.log("from logout API", _id);
   return new Promise(async (resolve, reject) => {
     try {
       const { data } = await axios.post(catApi, _id);
